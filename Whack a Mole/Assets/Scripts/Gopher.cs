c222f376fa359519eeb97a513dbbc30acb5ec433
Audrey Schmitt <audrey.schmitt07@gmail.com>
2020-07-20 15:13:03 -0400

    Basic Mole / MoleSpawner behaviour

---
+++ Whack a Mole/Assets/Scripts/Gopher.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gopher : MonoBehaviour
+{
+
+    private IEnumerator waitAndDestroy;
+    private int lifetime;
+    
+    public Gopher(int lifetime) {
+
+        this.lifetime = lifetime;
+
+    }
+
+    public Gopher() {
+
+        lifetime = 2;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       waitAndDestroy = WaitAndDestroy();
+       StartCoroutine(waitAndDestroy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    // Destroy the object if clicked
+    void OnMouseDown() {
+
+        Destroy(this.gameObject);
+
+    }
+
+    IEnumerator WaitAndDestroy() {
+
+        yield return new WaitForSeconds(lifetime);
+
+        // Destroy gopher if not clicked during lifetime
+        Destroy(this.gameObject);
+
+    }
+}
---
+++ Whack a Mole/Assets/Scripts/GopherSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GopherSpawner : MonoBehaviour
+{
+    // GopherSpawner handles spawning / despawning of Gophers
+
+    public GameObject gopher;
+    public GameObject gopherParent;
+    private IEnumerator waitAndSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Activate the GameObject -- start spawning gophers
+        waitAndSpawn = WaitAndSpawn(3, 8);
+        StartCoroutine(waitAndSpawn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // Method to pick a random time between 'min' and 'max' seconds, calls 'SpawnGopher()' after selected number of seconds
+    IEnumerator WaitAndSpawn(float min, float max) {
+
+        while (true) {
+
+            // Wait for selected time to finish
+            yield return new WaitForSeconds(Random.Range(min,max));
+
+            // Spawn a Gopher
+            SpawnGopher();
+
+        }
+
+    }
+
+    // Method to spawn a gopher - called from RandomTime
+    void SpawnGopher() {
+
+        Instantiate(gopher, new Vector3(transform.position.x, transform.position.y, transform.position.z - 3), transform.rotation, gopherParent.transform);
+        Debug.Log("Gopher Spawned!");
+
+    }
+}
