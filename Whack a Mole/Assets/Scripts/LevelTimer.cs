b029428c201ae7d0ef3bca7e13edc31630e0935e
Audrey Schmitt <audrey.schmitt07@gmail.com>
2020-09-15 17:42:49 -0400

    Split Time functionality into two classes -- LevelTimer and LevelTimerUI

---
+++ Whack a Mole/Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTimer : MonoBehaviour
+{
+    [SerializeField] private int levelTimeInSeconds = 0;
+    
+    private int _secondsSinceStart = 0;
+    private LevelTimerUI _levelTimerUI;
+
+    private void Start()
+    {
+        _levelTimerUI = GameObject.FindGameObjectWithTag("LevelTimer").GetComponent<LevelTimerUI>();                    // Get a reference to and set up the LevelTimerUI
+        _levelTimerUI.Setup(levelTimeInSeconds);
+
+        //TODO start the timer for now, later move this responsibility into a LevelManager class
+        StartTimer();
+    }
+    
+    private void StartTimer()
+    {
+        InvokeRepeating(nameof(LevelTimer.UpdateTimer), 1f, 1f);                                          // Start updating the timer
+    }
+
+    private void StopTimer()
+    {
+        CancelInvoke();
+    }
+    
+    private void UpdateTimer()
+    {
+        _secondsSinceStart ++;                                                                                           // Update the display every second
+        _levelTimerUI.UpdateDisplay(_secondsSinceStart);
+
+        if (_secondsSinceStart == levelTimeInSeconds)                                                                    // Level has run out of time, stop timer
+        {
+            StopTimer();
+        }
+    }
+}
---
+++ Whack a Mole/Assets/Scripts/LevelTimerUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelTimerUI : MonoBehaviour
+{
+    private Slider _slider;
+
+    public void UpdateDisplay(int value)
+    {
+        _slider.value = value;
+    }
+
+    public void Setup(int levelLengthInSeconds)
+    {
+        _slider = gameObject.GetComponent<Slider>();
+        _slider.maxValue = levelLengthInSeconds;
+    }
+}
